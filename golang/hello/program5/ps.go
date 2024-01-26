204f3490c53990769b0d83b88274c74e2dc676b7
vardhanjavvadhi <134599754+vardhanjavvadhi@users.noreply.github.com>
2024-01-26 16:41:53 +0530

    Update ps.go

--- golang/hello/program5/ps.go
+++ golang/hello/program5/ps.go
@@ -23,30 +23,3 @@ fmt.Printf( "Hello, %s! You are %d years old.\n", name,age)
 
 	fmt.Printf("Nice to meet you, %s! You are %d years old.\n ", inputName, inputage)
 }
-//Golang program to demonstrate the use of 
-//printf() and scanf() function.
-
-// package main
-// import "fmt"
-
-// func main() {
-//     //Declare 4 integer type variables.
-//     var p int
-//     var r int
-//     var t int
-//     var si int
-    
-    
-//     fmt.Print("Enter principal: " ) 
-//     fmt.Scanf("%d\n",&p)
-    
-//     fmt.Print("Enter rate: ") 
-//     fmt.Scanf("%d\n",&r)
-    
-//     fmt.Print("Enter time: ") 
-//     fmt.Scanf("%d\n",&t)
-    
-//     si=(p*r*t)/100
-    
-//     fmt.Println("Simple interest is: ",si) 
-// }
